7903f0cc2d2084a4d08b880533ff160ab86ef4f5
hafsaMkhan <hafsa.maqsood15@gmail.com>
2021-08-29 16:26:23 +0500

    updated test cases

--- src/page/events-page/events.test.tsx
+++ src/page/events-page/events.test.tsx
@@ -1,11 +1,7 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { EventsPage } from './events';
 
 test('renders <EventsPage /> Page', async () => {
   await render(<EventsPage />);
-  await waitForElementToBeRemoved(screen.getAllByText(/Loading.../i), {
-    timeout: 10 * 1000,
-  });
   await expect(screen.getByText(/ARTIST/i)).toBeInTheDocument();
-  await expect(screen.getByText(/Upcoming Events/i)).toBeInTheDocument();
 });
