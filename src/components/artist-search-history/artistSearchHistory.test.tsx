04e3871ff1746791e58c5aa538745d11310d20f8
hafsaMkhan <hafsa.maqsood15@gmail.com>
2021-08-29 15:45:56 +0500

    added test cases

--- src/components/artist-search-history/artistSearchHistory.test.tsx
+++ src/components/artist-search-history/artistSearchHistory.test.tsx
@@ -2,9 +2,14 @@ import { render, screen } from '@testing-library/react';
 import { ArtistSearchHistory } from './index';
 
 describe('<ArtistSearchHistory /> tests', () => {
-  it('renders the ArtistSearchHistory component', () => {
+  beforeEach(() => {
     render(<ArtistSearchHistory />);
+  });
+  it('renders the ArtistSearchHistory component', () => {
     const linkElement = screen.getByText(/Search History/i);
     expect(linkElement).toBeInTheDocument();
+  });
+  it('rendering data from cache', () => {
+    expect(screen.getByText(/No Search Results/i)).toBeInTheDocument();
   });
 });
